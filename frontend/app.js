a0508e89677fe07d4a19a6f9e101ccee871e981a
naugtur <naugtur@gmail.com>
2024-10-01 15:57:59 +0200

    fixup

--- frontend/app.js
+++ frontend/app.js
@@ -3,9 +3,14 @@ import { randomQuote } from "cookie-monster";
 import { reaction } from "./omg.ts";
 
 function run() {
+  // This is my cookie
   document.cookie = "name=chocolate-chip";
   console.log(location.host, document.cookie);
+
+  // These are my dependencies
   console.log(leftpad(randomQuote(), 50, "🍪"));
+
+  // This just proves webpack still does its thing
   console.log(reaction)
 }
 run();
